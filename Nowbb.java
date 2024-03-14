ac2d241e9f05a87c8f450f1ea84566768b7efe5e
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-14 22:02:55 +0530

    Exp

---
+++ Nowbb.java
@@ -0,0 +1,14 @@
+package First_Source;
+
+
+/**
+ * Nowbb
+ */
+public class Nowbb {
+
+    public static void main(String[] args) {
+        System.out.println("Working as a Calling agent in FS");
+
+        System.out.println("Will work till 6 months");
+    }
+}

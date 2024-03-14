c84db0abbec017cdd1966a1a67e71d767f1d140f
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-14 21:48:43 +0530

    Honesty

--- RGI.java
+++ RGI.java
@@ -7,5 +7,6 @@ public class RGI {
 
     public static void main(String[] args) {
         System.out.println("Working as a Email Agent at RGI");
+        System.out.println("Will going to spend only 6 months");
     }
 }
